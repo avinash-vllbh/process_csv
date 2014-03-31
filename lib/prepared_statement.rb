4d44ba67109875c4adf93d1973dc87979b04e2be
avinash-vllbh <avinash.vllbh@gmail.com>
2014-03-31 10:34:17 -0700

    Added module to output import command

--- lib/prepared_statement.rb
+++ lib/prepared_statement.rb
@@ -20,29 +20,31 @@ class String
 ##
 ###
 class PreparedStatement
-	def prepare_statement(filename)		
-			tbl_name = "Input".downcase
-			sql_string = "CREATE TABLE "+tbl_name.to_s+" (id INT NOT NULL AUTO_INCREMENT, "
+	def tbl_prepare_statement(filename)		
+			tbl_name = "Input".downcase # make this based on the filename
+			sql_string = "CREATE TABLE "+tbl_name.to_s+" ( "
 		if File::exists?(filename)
 			line_no = 0
 			CSV.foreach(filename) do |line|
-				print line[2]
-				puts "\n"
+				empty_values = line[6]
+				#data_type    = line[2]
+				#column_name  = line[1]
+        
 				if line_no > 2
 					if line[2] == "int"
-						if line[6] == "Not Empty"
+						if empty_values == "Not Empty"
 							sql_string = sql_string+"#{line[1]} "+line[2].upcase+" NOT NULL, "
 						else
 							sql_string = sql_string+line[1]+" "+line[2].upcase+", "
 						end
 					elsif line[2] == "date"
-						if line[6] == "Not Empty"
+						if empty_values == "Not Empty"
 							sql_string = sql_string+line[1]+" "+line[2].upcase+" NOT NULL, "
 						else
 							sql_string = sql_string+line[1]+" "+line[2].upcase+", "
 						end
 					else
-						if line[6] == "Not Empty"
+						if empty_values == "Not Empty"
 							sql_string = sql_string+line[1]+" varchar NOT NULL, "
 						else
 							sql_string = sql_string+line[1]+" varchar, "
@@ -57,8 +59,20 @@ class PreparedStatement
 			return FileNotFound.new
 		end
 	end
+	def csv_import_statement(filename,delimiter)
+		#LOAD DATA INFILE '/tmp/test.txt' INTO TABLE test IGNORE 1 LINES;
+		file = "#{filename}"
+		puts file
+		filename.slice!(/^processed_/)
+		tbl_name = File.basename(filename,".*")
+		import_statement = "LOAD DATA INFILE #{file} INTO TABLE #{tbl_name} "+
+							"FIELDS TERMINATED BY '#{delimiter}' "+
+							"ENCLOSED BY '\"' "+
+							"LINES TERMINATED BY '\\n' "+
+							"IGNORE 1 LINES;"
+	end
 end
 
-prep_stmt = PreparedStatement.new
-file = '../spec/prepared_statement/test.csv'
-prep_stmt.prepare_statement(file)
+# prep_stmt = PreparedStatement.new
+# file = '../spec/prepared_statement/test.csv'
+# prep_stmt.prepare_statement(file)
