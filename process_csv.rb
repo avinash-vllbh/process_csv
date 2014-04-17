2303aa8eac9ea98761bb16929073da4dfbe6378e
avinash-vllbh <avinash.vllbh@gmail.com>
2014-04-17 14:58:03 -0700

    Added additional options to command line inputs - replace_quotes and replace_nulls

--- process_csv.rb
+++ process_csv.rb
@@ -1,10 +1,11 @@
 require 'optparse'
+require 'tco'
 require_relative './lib/csv_processor'
 require_relative './lib/col_seperator'
 require_relative './lib/prepared_statement'
 require_relative './lib/csv_cleaner'
 
-options = {:input => nil, :metadata_output => nil, :processed_input => nil, :unique => 10, :chunk => 20, :skip => 0}
+options = {:input => nil, :metadata_output => nil, :processed_input => nil, :unique => 10, :chunk => 20, :skip => 0, :database => "P", :quote_convert => "YES", :replace_nulls => "NO"}
 parser = OptionParser.new do |opts|
 			opts.banner = "Usage: process_csv.rb [options]"
 
@@ -26,6 +27,15 @@ parser = OptionParser.new do |opts|
 			opts.on('-s', '--skip lines', 'skip the number of lines at the top, default: 0') do |skip|
 				options[:skip] = skip
 			end
+			opts.on('-d', '--database type', 'MySQL or Postgres, Options: M or P, default: P') do |database_type|
+				options[:database] = database_type.upcase
+			end
+			opts.on('-q', '--quotes conversion', 'Convert single quotes to double quotes, options: yes or no, default: yes') do |quote_convert|
+				options[:quote_convert] = quote_convert.upcase
+			end
+			opts.on('-r', '--replace nulls', 'replace empty, Null\'s, \N, NAN with NULL, options: yes or no, default: yes') do |replace_nulls|
+				options[:replace_nulls] = replace_nulls.upcase
+			end
 			opts.on('-h', '--help', 'Displays Help') do
 				puts opts
 				exit
@@ -42,72 +52,154 @@ if options[:input] == nil
 end 
 
 flag = false
-if(Integer(options[:unique]) rescue false)
+if(Integer(options[:unique]) rescue false && options[:unique] >= 0)
 else
 	while not flag
-		puts "Not a valid number for unique. Enter an integer"
+		puts "Not a valid number for unique. Enter a positive integer"
 		options[:unique] = gets.chomp
 		puts options[:unique]
-		if(Integer(options[:unique]) rescue false)
+		if(Integer(options[:unique]) rescue false && options[:unique] >= 0)
 			flag = true		
 		end
 	end
 end
 flag = false
-if(Integer(options[:chunk]) rescue false)
+if(Integer(options[:chunk]) rescue false && options[:chunk] >= 0)
 else
 	while not flag
-		puts "Not a valid number for chunk size. Enter an integer"
+		puts "Not a valid number for chunk size. Enter a positive integer"
 		options[:chunk] = gets.chomp
 		puts options[:chunk]
-		if(Integer(options[:chunk]) rescue false)
+		if(Integer(options[:chunk]) rescue false && options[:chunk] >= 0)
 			flag = true		
 		end
 	end
+end
+if options[:database_type] == "M" || options[:database_type] == "MYSQL"
+	options[:database_type] = "M"
+elsif options[:database_type] == "P" || options[:database_type] == "POSTGRESSQL" || options[:database_type] == "POSTGRES"
+	options[:database_type] = "P"
+else
+	while options[:database_type] == "M" || options[:database_type] == "P"
+		puts "Invalid option for Database Type. Enter M for MySQL or P for PostgresSQL".fg("#ff0000")
+		options[:database_type] = gets.chomp.upcase
+	end
+end
+#To verify the skip_lines argument data integrity
+if options[:skip] >= 0
+	skip_lines = options[:skip]
+else
+	while options[:skip] >= 0
+		puts "Wrong input for skip lines! Enter a positive integer"
+		options[:skip] = gets.chomp
+	end
+	skip_lines = options[:skip]
 end
 
 input_file = options[:input]
 chunk_size = options[:chunk]
 no_of_unique = options[:unique]
-skip_lines = options[:skip]
+quotes_convert = options[:quote_convert]
+replace_nulls = options[:replace_nulls]
+
+replace_nulls = "YES" if replace_nulls == ""
+replace_nulls = "YES" if replace_nulls == "Y"
+replace_nulls = "NO" if replace_nulls == "N"
+while replace_nulls != "YES" && replace_nulls != "NO"
+	puts "Invalid input!! Enter either (Yes/no)".fg("#ff0000")
+	replace_nulls = gets.chomp.upcase
+	replace_nulls = "YES" if replace_nulls == ""
+	replace_nulls = "YES" if replace_nulls == "Y"
+	replace_nulls = "NO" if replace_nulls == "N"
+end
+quotes_convert = "YES" if quotes_convert == ""
+quotes_convert = "YES" if quotes_convert == "Y"
+quotes_convert = "NO" if quotes_convert == "N"
+while quotes_convert != "YES" && quotes_convert != "NO"
+	puts "Invalid input!! Enter either (Yes/no)".fg("#ff0000")
+	quotes_convert = gets.chomp.upcase
+	quotes_convert = "YES" if quotes_convert == ""
+	quotes_convert = "YES" if quotes_convert == "Y"
+	quotes_convert = "NO" if quotes_convert == "N"
+end
+
 
 if File::exists?(input_file)
 
-	#puts File.basename(input_file)
-	#puts File.absolute_path(input_file)
-	if options[:metadata_output] == nil
-		output_file = Dir.pwd+"/output.csv"
-	else
-		output_file = options[:metadata_output]
-	end
-	#puts output_file
-	#Obtain the delimeter
-	col_sep = ColSeperator.new
-	delimiter = col_sep.get_delimiter(input_file)
-	if delimiter == "\t"
-		puts "Delimiter in given input is Tab"
-	else
-		puts "Delimiter in given input is #{delimiter}"
-	end
-	if options[:processed_input] == nil
-		processed_file_name = "processed_"+File.basename(input_file)
-	else
-		processed_file_name = options[:processed_input]
-	end
-	csv_clean = CSVCleaner.new
-	csv_clean.cleaner_csv(input_file,delimiter,processed_file_name,skip_lines)
+	if(File.extname(input_file) == ".csv" || File.extname(input_file) == ".dat")
 
-	csv_process = CSVProcessor.new
-	csv_process.get_header_length(processed_file_name,delimiter)
-	csv_process.initial_data_type(processed_file_name,chunk_size,delimiter)
-	csv_process.process_csv_file(processed_file_name, no_of_unique,delimiter)
-	csv_process.output_csv(output_file, no_of_unique)
+		#puts File.basename(input_file)
+		#puts File.absolute_path(input_file)
+		if options[:metadata_output] == nil
 
-	prep_stat = PreparedStatement.new
-	sql_query_result = prep_stat.tbl_prepare_statement(output_file,input_file)
-	import_query = prep_stat.csv_import_statement(input_file,delimiter)
-	puts sql_query_result
-	puts import_query
+			output_file = Dir.pwd+"/"+"metadata_"+File.basename(input_file)
+		else
+			output_file = options[:metadata_output]
+		end
+		if options[:processed_input] == nil
+			processed_file_name = "processed_"+File.basename(input_file)
+		else
+			processed_file_name = options[:processed_input]
+		end
+
+		#To clean the line endings
+		#Replace CR (\r) used as line endings with \n
+		file_base = File.basename(input_file)
+		file_dir = File.dirname(processed_file_name)
+		new_input_file = "#{file_dir}/new.#{file_base}".to_s
+		#puts new_input_file
+		csv_clean = CSVCleaner.new
+		csv_clean.clean_line_endings(input_file,new_input_file)
+		input_file = new_input_file
+		#puts output_file
+		#Obtain the delimeter
+		input_file = new_input_file
+		col_sep = ColSeperator.new
+		delimiter = col_sep.get_delimiter(input_file)
+		if delimiter == "\t"
+			puts "Delimiter in given input is Tab"
+		else
+			puts "Delimiter in given input is #{delimiter}"
+		end
+
+		
+		csv_clean.cleaner_csv(input_file,delimiter,processed_file_name,skip_lines,replace_nulls,quotes_convert)
+
+		csv_process = CSVProcessor.new
+		csv_process.get_header_length(processed_file_name,delimiter)
+		csv_process.initial_data_type(processed_file_name,chunk_size,delimiter)
+		csv_process.process_csv_file(processed_file_name, no_of_unique,delimiter)
+		csv_process.output_csv(output_file, delimiter,no_of_unique,replace_nulls,quotes_convert)
+
+		prep_stat = PreparedStatement.new
+		my_sql_query,pg_sql_query = prep_stat.tbl_prepare_statement(output_file,input_file)
+		my_import_query,pg_import_query = prep_stat.csv_import_statement(processed_file_name,delimiter,skip_lines)
+
+		CSV.open(output_file, "a+") do |csv|
+			csv << []
+			csv << ["SQL Commands"]
+			csv << ["Database Type", "MYSQL"]
+			csv << ["CREATE TABLE STMT",my_sql_query]
+			csv << ["IMPORT PROCESSED FILE STMT", my_import_query]
+			csv << ["Database Type", "POSTGRES"]
+			csv << ["CREATE TABLE STMT",pg_sql_query]
+			csv << ["IMPORT PROCESSED FILE STMT", pg_import_query]			
+		end
+		if options[:database] == "M"
+			puts "MySQL".fg("#ff0000")
+			puts my_sql_query
+			puts my_import_query
+		else
+			puts "PostgresSQL".fg("#ff0000")
+			puts pg_sql_query
+			puts pg_import_query
+		end
+
+		#to delete the new.input file created during the process
+		#File.delete(input_file)
+	else
+		puts "Please export the input file to CSV or TSV".fg("#ff0000")
+	end
 else
 	puts "No such input file exists!!"
 	puts parser
